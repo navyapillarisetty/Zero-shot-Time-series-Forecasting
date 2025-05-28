19977f94b79b12e714520bd52c1ad7e1e0981664
navyapillarisetty <130261532+navyapillarisetty@users.noreply.github.com>
2025-05-27 23:36:17 -0700

    Add files via upload

---
+++ statistical_information/summary_stat.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def sum_stat(data,seq_length):
+    reshap_data= data.reshape((len(data))//seq_length,seq_length,1)
+    mean_arr= []
+    std_arr= []
+    for i in range(len(reshap_data)):
+        mean_arr.append(np.mean(reshap_data[i]))
+        std_arr.append(np.std(reshap_data[i]))
+    print(mean_arr)
+    print(len(mean_arr))
+    return mean_arr, std_arr
+
+def random_data(data,seq_length):
+    stat_info= []
+    mean, std= sum_stat(data, seq_length)
+    for i in range(len(mean)):
+        stat_info.append(np.random.normal(mean[i],std[i], seq_length))
+    stat_info= np.array(stat_info)
+    stat_info1= stat_info.reshape(len(stat_info)*seq_length)
+    stat_info1= np.array(stat_info1)
+    return stat_info1
+
