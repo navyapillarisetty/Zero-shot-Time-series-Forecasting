798990d828c30cffc6f566c82028a47fbd0d493d
navyapillarisetty <130261532+navyapillarisetty@users.noreply.github.com>
2026-09-22 20:57:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,193 +8,86 @@ from statistical_information.IDEALEM import encoder
 # if statistical information from IDEALEM is to be used 
 numberOfbuffers= 255
 exchan= encoder(seq_length, numberOfbuffers)
-y= []
-for j in exchan:
-    j= j-4
-    if j>=0:
-        y.append(j)
-
-
-
-import subprocess
-
-# Only in case of Informer, comment out the following code
-# subprocess.run([
-#     "python",
-#     "./Informer/main_informer.py",
-#     "--model", "informer",
-#     "--data", "Part2",
-#     "--features", "S",
-#     "--attn", "prob",
-#     "--do_predict"
-# ])
-
-
-# post processing steps
 
 import numpy as np
 import pandas as pd
-import torch
-import math
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
+import os
+import subprocess
 
+sequence_len= 24
 
-# the statistical information data path
-#data_path= "../L1MAG_part2_decoded.csv.bin.idealem.bin"
-data_path= "./statistical_information/stat_info_results/L1MAG_part2_summary_statistics.csv"
-
-if data_path.endswith(".csv"):
-    hint= np.loadtxt(data_path)
-elif data_path.endswith(".bin"):
-    hint= np.fromfile(data_path)
-else:
-    hint= pd.read_csv(data_path,header=0)
-
-hint1= pd.DataFrame(hint)
-
-prediction= np.loadtxt("./results/L1MAG_part2_preds.csv")
-
-class StandardScaler():
-    def __init__(self):
-        self.mean = 0.
-        self.std = 1.
-
-    def fit(self, data):
-        self.mean = data.mean(0)
-        self.std = data.std(0)
-
-    def transform(self, data):
-        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
-        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
-        return (data - mean) / std
-
-    def inverse_transform(self, data):
-        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
-        print(mean)
-        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
-        print(std)
-        mean1= pd.Series(mean)
-        print(data.shape)
-        print(mean1.shape)
-        if data.shape[-1] != mean1.shape[-1]:
-            mean = mean[-1:]
-            std = std[-1:]
-        return (data * std) + mean
+def merging(pred,hint_i):
+    pred= np.array(pred)
+    hint_i= np.array(hint_i)
+    pred1= pred.reshape(1*sequence_len*1)
+    index= np.argsort(pred1)
+    hint1= hint_i.reshape(1*sequence_len*1)
+    sorted_hint= list(sorted(hint1))
+    merged= list(np.zeros(len(sorted_hint)))
+    for i in range(0,len(sorted_hint)):
+        merged[index[i]]= sorted_hint[i]
+    merged= np.array(merged)
+    return merged
     
 
-# since informer is predictive model last sequence length block is removed 
-hint_x= hint1
-scaler= StandardScaler()
-scaler.fit(hint_x)
-pred= pd.DataFrame(prediction)
-pred1= scaler.inverse_transform(pred)
-pred1= np.array(pred1)
-pred1= pred1[:(len(pred1)-seq_length)]
+def IDEALEM_informer(train_data: str,test_data: str, hint: str):
+    testing_data_loc= f"./Data/{test_data}.csv"
+    #testing_data= np.loadtxt(testing_data_loc)
+    for j in range(0,5,1):
+        testing_data= np.loadtxt(testing_data_loc)
+        overall_prediction= []
+        overall_merged=[]
+        test_data1_loc= f"./Data/{test_data}_{j}.csv"
+        np.savetxt(test_data1_loc,testing_data)
+        test_data1= f"{test_data}_{j}"
+        hint_data_loc= f"./Data/{hint}_part2_decoded_incomplete.csv.bin"
+        #hint_data_loc= f"./Data/{hint}_part2_stat_info.csv"
+        hinting= np.fromfile(hint_data_loc)
+        for i in range(0,157,1):
+            subprocess.run(["python","../Informer2020-main/main_informer.py",
+            "--model", "informer",
+            "--data", "Part2",
+            "--features", "S",
+            "--attn", "prob",
+            "--train_data_path", train_data,
+            "--test_data_path", test_data1,
+            "--label_len","48",
+            "--train_epochs","6",
+            "--do_predict"])
+            block_pred= np.loadtxt(r"./Pred/pred_results.csv")
+            testing=testing_data[sequence_len:]
+            hinting1= hinting[i*sequence_len:(i*sequence_len)+sequence_len]
+            merged_dat= merging(block_pred,hinting1)
+            testing1= np.append(testing,merged_dat)
+            testing_data= testing1
+            np.savetxt(test_data1_loc,testing_data)
+            overall_prediction.append(block_pred)
+            overall_merged.append(merged_dat)
+        overall_prediction= np.array(overall_prediction)
+        overall_merged= np.array(overall_merged)
+        if train_data=="ETTh1_train":
+            prediction_loc= f"./Pred/{train_data}_trained_{hint}_test_{j}.csv"
+            merged_loc= f"./Pred/merged_pred/{train_data}_trained_{hint}_test_{j}.csv"
+        else:
+            prediction_loc= f"./Pred/{train_data}_trained_{test_data}_{j}.csv"
+            merged_loc= f"./Pred/merged_pred/{train_data}_trained_{test_data}_{j}.csv"
+        np.savetxt(prediction_loc,overall_prediction)
+        np.savetxt(merged_loc,overall_merged)
+        print("#"*150, f"End of round {j}")
+        
+train_data= ["L1MAG_train","L2MAG_train","L3MAG_train","LIC_train","gold_price_train","bike_rentals_train","ETTh1_train"]
+test_data= ["L1MAG_test","L2MAG_test","L3MAG_test","LIC_test","gold_price_test","bike_rentals_test","ETTh1_test"]
+hint_data= ["L1MAG","L2MAG","L3MAG","LIC","gold_price","bike_rentals","ETTh1"]
 
-pred2= pred1.reshape((len(pred1))//seq_length,seq_length,1)
-
-fake= []
-length= len(pred2)
-for i in range(0,length,seq_length):
-    dat2= pred2[i]
-    fake.append(dat2)
-print(len(fake))
-
-original= np.fromfile("./Data/L1MAG_part2.csv.bin", dtype= np.float64)
-original= original[seq_length*4:]
-original= np.array(original)
-original1= original.reshape((len(original)//seq_length),seq_length,1)
-
-fake= np.array(fake)
-
-
-original=[]
-synthetic_n=[]
-for i in range(len(y)):
-    ori= original1[y[i]]
-    syn= fake[y[i]]
-    original.append(ori)
-    synthetic_n.append(syn)
-#print(len(original))
-#print(len(synthetic_n))
-
-original= np.array(original)
-synthetic= np.array(synthetic_n)
-ori= original.reshape((len(original))*seq_length)
-syn= synthetic.reshape((len(synthetic))*seq_length)
-
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-
-figure(figsize=(20,5), dpi=80)
-plt.plot(ori, color="red", label="Ground truths")
-plt.plot(syn, color= "blue", label="Before merging")
-plt.grid()
-plt.title("Before merging plot")
-plt.legend()
-plt.show()
-
-def SMAPE(Y_real, Y_fake):
-    smape= np.mean((np.abs(Y_real - Y_fake))/(abs(Y_real) + abs(Y_fake)))*100
-    return smape
-
-mse= mean_squared_error(ori, syn)
-mae= mean_absolute_error(ori, syn)
-smape= SMAPE(ori, syn)
-print("Before merging error")
-print("mse: ",mse, "mae: ", mae)
-print("smape: ",smape)
-
-decoded1= hint
-decoded= decoded1.reshape((len(decoded1))//seq_length,seq_length)
-decode= decoded[4:]
-
-dec=[]
-for i in range(len(y)):
-    decoding= decode[y[i]]
-    dec.append(decoding)
-print(len(dec))
-
-index_data=[]
-for g in synthetic_n:
-    arr= np.array(g)
-    arr1= arr.reshape(1*seq_length*1)
-    index= np.argsort(arr1)
-    index_data.append(index)
-sorted_data= []
-for h in dec:
-    decoded2= np.array(h)
-    decoded3= decoded2.reshape(1*seq_length*1)
-    #print(decoded3)
-    sorted_dec= list(sorted(decoded3))
-    sorted_data.append(sorted_dec)
+for k in range(len(train_data)):
+    if train_data[k] == "ETTh1_train":
+        for l in range(len(hint_data)):
+            print("ETTh1_train",hint_data[l])
+            IDEALEM_informer("ETTh1_train","ETTh1_test",hint_data[l])
+    else:
+        print(train_data[k],test_data[k])
+        IDEALEM_informer(train_data[k],test_data[k],hint_data[k])
 
 
-merged_data=[]
-for u in range(len(index_data)):
-    index1= index_data[u]
-    sort= sorted_data[u]
-    merged= list(np.zeros(len(sort)))
-    for i in range(0,len(sort)):
-        merged[index1[i]]= sort[i]
-    merged_data.append(merged)
-merged_data= np.array(merged_data)
-merged_data1= merged_data.reshape((len(merged_data))*seq_length)
 
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
 
-figure(figsize=(20,5), dpi=80)
-plt.plot(ori, color="red", label="Ground truths")
-plt.plot(merged_data1, color= "blue", label="After merging")
-plt.title("After merging plots")
-plt.legend()
-plt.show()
-
-mse1= mean_squared_error(ori, merged_data1)
-mae1= mean_absolute_error(ori, merged_data1)
-smape1= SMAPE(ori, merged_data1)
-print("merged error")
-print("mse: ",mse1, "mae: ", mae1)
-print("smape: ",smape1)
