47afd4b0f0cddd6a693f371b61e12c507e394b60
navyapillarisetty <130261532+navyapillarisetty@users.noreply.github.com>
2026-09-22 20:35:35 -0400

    Update main_informer.py

--- Informer/main_informer.py
+++ Informer/main_informer.py
@@ -9,6 +9,9 @@ parser = argparse.ArgumentParser(description='[Informer] Long Sequences Forecast
 parser.add_argument('--model', type=str, required=True, default='informer',help='model of experiment, options: [informer, informerstack, informerlight(TBD)]')
 
 parser.add_argument('--data', type=str, required=True, default='Part2', help='data')
+#parser.add_argument('--root_path', type=str, default=r'./Data/', help='root path of the data file')
+parser.add_argument('--train_data_path', type=str, required=True, help='train data name')
+parser.add_argument('--test_data_path', type=str, required=True, help= 'test data name')
 parser.add_argument('--root_path', type=str, default=r'./Data/', help='root path of the data file')
 parser.add_argument('--features', type=str, default='M', help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
 parser.add_argument('--target', type=str, default='Data', help='target feature in S or MS task')
@@ -42,7 +45,7 @@ parser.add_argument('--mix', action='store_false', help='use mix attention in ge
 parser.add_argument('--cols', type=str, nargs='+', help='certain cols from the data files as the input features')
 parser.add_argument('--num_workers', type=int, default=0, help='data loader num workers')
 #parser.add_argument('--itr', type=int, default=2, help='experiments times')
-parser.add_argument('--itr', type=int, default=2, help='experiments times')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
 #parser.add_argument('--train_epochs', type=int, default=6, help='train epochs')
 parser.add_argument('--train_epochs', type=int, default=6, help='train epochs')
 parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
@@ -80,11 +83,12 @@ data_parser = {
     'Solar':{'data':'solar_AL.csv','T':'POWER_136','M':[137,137,137],'S':[1,1,1],'MS':[137,137,1]},
     'L1MAG':{'data':'BANK514L1MAG3.csv.bin','T':'Data','S':[1,1,1]},
     'Rough': {'data':'rough_data.csv','T':'Data', 'S':[1,1,1]},
-    'Part2' : {'data':'ETTh1.csv.bin','T':'Data', 'S':[1,1,1]}
+    'Part2' : {'train_data':'L1MAG_train','test_data':'L1MAG_test', 'T':'Data', 'S':[1,1,1]}
 }
 if args.data in data_parser.keys():
     data_info = data_parser[args.data]
-    args.data_path = data_info['data']
+    args.train_data_path = data_info['train_data']
+    args.test_data_path= data_info['test_data']
     args.target = data_info['T']
     args.enc_in, args.dec_in, args.c_out = data_info[args.features]
 
